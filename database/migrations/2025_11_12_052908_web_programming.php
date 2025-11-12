9fd5d205629fa8aa387221f794459c7377c4ab1e
muhzain <mzhasan1404@gmail.com>
2025-11-12 12:33:18 +0700

    first commit

---
+++ database/migrations/2025_11_12_052908_web_programming.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        // PAGES
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->longText('content');
+            $table->string('featured_image')->nullable();
+            $table->string('meta_title')->nullable();
+            $table->string('meta_description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+
+        // NEWS
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('excerpt')->nullable();
+            $table->longText('content');
+            $table->string('thumbnail')->nullable();
+            $table->dateTime('published_at')->nullable();
+            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->enum('status', ['draft', 'published'])->default('draft');
+            $table->timestamps();
+        });
+
+        // EVENTS
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->longText('description');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date')->nullable();
+            $table->string('location')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->timestamps();
+        });
+
+
+        // SETTINGS
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('key')->unique();
+            $table->text('value')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+        Schema::dropIfExists('events');
+        Schema::dropIfExists('category_news');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('news');
+        Schema::dropIfExists('pages');
+    }
+};
